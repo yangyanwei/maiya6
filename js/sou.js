edbfbefc9c6d0a6e478e7c0b7f661ecb140aa302
yangyanwei <1423676799@qq.com>
2016-04-01 13:51:47 +0800

    Update sou.js

--- js/sou.js
+++ js/sou.js
@@ -3,7 +3,27 @@ $(function(){
         location.href="../html/sousuokuang.html"
     });
 
+$('.cnv3').click(function(){
+        $('.cnv').css({'color': '#9c9c9c','background':'none','border-radius': 'none'});
+        $(this).css({'color': '#4e4e4e','background':'#ececec','border-radius': '20px'})
+    });
 
+    $('.cnv').click(function(){
+        $('.cnv').css({'color': '#9c9c9c','background':'none','border-radius': 'none'});
+        $('.cnv3').css({'color': '#9c9c9c','background':'none','border-radius': 'none'});
+        $(this).css({'color': '#4e4e4e','background':'#ececec','border-radius': '20px'})
+    });
+
+    $('.cnv2').click(function(){
+        $('.cnv1').css({'color': '#9c9c9c','background':'none','border-radius': 'none'});
+        $(this).css({'color': '#4e4e4e','background':'#ececec','border-radius': '20px'})
+    });
+
+    $('.cnv1').click(function(){
+        $('.cnv1').css({'color': '#9c9c9c','background':'none','border-radius': 'none'});
+        $('.cnv2').css({'color': '#9c9c9c','background':'none','border-radius': 'none'});
+        $(this).css({'color': '#4e4e4e','background':'#ececec','border-radius': '20px'})
+    });
 
 
 
